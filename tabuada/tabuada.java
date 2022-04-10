73069c7d8a470d49213acd100639ad68e9bc2adc
Anna Beatriz <86749217+annaX0@users.noreply.github.com>
2022-04-10 02:15:19 +0000

    Tabuada 9

---
+++ tabuada/tabuada.java
@@ -0,0 +1,8 @@
+class tabuada {
+  public static void main(String[] args) {
+    for (int i = 0; i < 11; i++) {
+      System.out.println("O valor é igual: " + (i) + " x 7 " + " = " + (i * 7));
+
+    }
+  }
+}
